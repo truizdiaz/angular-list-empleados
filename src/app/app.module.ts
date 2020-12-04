dd826ab54043e7c8b0863912d24052e4b46c41d6
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-04 10:06:02 -0300

    Creacion de componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmpleadoListComponent } from './components/empleado-list/empleado-list.component';
+import { CountEmpleadosComponent } from './components/empleado-list/count-empleados/count-empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadoListComponent,
+    CountEmpleadosComponent
   ],
   imports: [
     BrowserModule
