013379ef9a5a3922eceef3777deee53153120e40
truizdiaz <tomiruizdiaz@gmail.com>
2020-12-04 10:54:19 -0300

    Get Total

--- src/app/components/empleado-list/empleado-list.component.ts
+++ src/app/components/empleado-list/empleado-list.component.ts
@@ -12,12 +12,23 @@ export class EmpleadoListComponent implements OnInit {
     { legajo: 2, nombre: 'Marcos', apellido: 'Gozalez', sexo: 'Masculino', salario: 65000 },
     { legajo: 3, nombre: 'Marta', apellido: 'Garcia', sexo: 'Femenino', salario: 75000 },
     { legajo: 4, nombre: 'Ignacio', apellido: 'Cortes', sexo: 'Masculino', salario: 55000 },
-    { legajo: 5, nombre: 'Maria', apellido: 'Navarro', sexo: 'Femenino', salario: 80000 },
-    { legajo: 6, nombre: 'Joaquin', apellido: 'Marquez', sexo: 'Masculino', salario: 80000 }
+    { legajo: 5, nombre: 'Maria', apellido: 'Navarro', sexo: 'Femenino', salario: 80000 }
   ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  obtenerTotalEmpleados(): number {
+    return this.listEmpleados.length;
+  }
+
+  obtenerTotalFemeninos(): number {
+    return this.listEmpleados.filter(list => list.sexo === 'Femenino').length;
+  }
+
+  obtenerTotalMasculinos(): number {
+    return this.listEmpleados.filter(list => list.sexo === 'Masculino').length;
   }
 
 }
